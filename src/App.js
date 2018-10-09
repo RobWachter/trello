263eb15a4f26ae7a11af886c8e0f1845df7b0016
Rob Wachter <robertewachter@gmail.com>
2018-10-09 11:02:04 -0400

    second push

--- src/App.js
+++ src/App.js
@@ -3,24 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+  constructor(props) {
+    super(props)
+
+    //enter state items here
+
+    this.state = {
+      image: "StreetFighter" // "MortalKombat"
+    }
+
+    this.drawImage = this.drawImage.bind(this)
+  }
+
+  drawImage() {
+    if (this.state.image === "StreetFighter")
+    {
+      return <img alt="tuesday" src="https://media.giphy.com/media/1192zFRwYEf4g8/giphy.gif" />
+    }
+    else
+    {
+      return <img alt="kano" src="https://media.giphy.com/media/elAmQxZBB0sJW/giphy.gif" />
+    }
+  }
+
+
+
+   render() {
+    // let someImage = "";
+    // if (this.state.image === "StreetFighter")
+    // {
+    //   someImage = <img alt="tuesday" src="https://media.giphy.com/media/1192zFRwYEf4g8/giphy.gif" />
+    // }
+    // else
+    // {
+    //   someImage = <img alt="kano" src="https://media.giphy.com/media/elAmQxZBB0sJW/giphy.gif" />
+    // }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <section className="container">
+        <div className="box" onClick={this.drawImage()}>
+        {/* {this.drawImage()} */}
+
+        </div>
+      </section>
     );
   }
 }
